3c1b434bd7d742463668e44d005357c35fca24a0
rivertec <oriver1129@gmail.com>
2022-01-24 18:10:03 -0500

    Practice 1, println and readLine // 실습 1, 인사말과 사용자로부터 입력 받은 정보 출력

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("Welcome, to the App")
+    println("Please enter your name")
+
+    val name = readLine()
+    println("""
+        Thank you, $name
+        Please select a category.
+        ***====================================***
+    """.trimIndent())
+
 }
