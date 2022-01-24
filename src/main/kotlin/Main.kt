f693cbf050c8251386fa32fbd1bb76465ad8d0fc
rivertec <oriver1129@gmail.com>
2022-01-24 18:47:26 -0500

    Practice 2, 상품 카테고리 메뉴의 사용자 입력값이 없는 경우 재입력 요청 메세지 출력 처리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,4 +10,27 @@ fun main(args: Array<String>) {
         ***====================================***
     """.trimIndent())
 
+    val categories = arrayOf("Fashion", "Electronics", "Pet Supplies")
+
+    for (category in categories) {
+        println(category)
+    }
+    println("=> Enter # to view cart")
+
+    var selectedCategory = readLine()
+
+    while (selectedCategory.isNullOrBlank()) {
+        println("Please select a category")
+        selectedCategory = readLine()
+    }
+
+    if(selectedCategory == "#") {
+        //TODO 1. move to the cart
+    } else {
+        //TODO 2. Show selected category
+
+    }
+
+
+
 }
